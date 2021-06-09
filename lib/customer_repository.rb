456ef56a6d29a377f9dfe46a8e5acd59d90c8a47
danembb <dbrophy720@gmail.com>
2021-06-09 16:53:18 -0600

    Complete customer repository and customer class

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -37,13 +37,13 @@ class CustomerRepository
 
   def find_all_by_first_name(first_name)
     @all.find_all do |customer|
-      customer.first_name == first_name
+      customer.first_name.downcase.include?(first_name.downcase)
     end
   end
 
   def find_all_by_last_name(last_name)
     @all.find_all do |customer|
-      customer.last_name == last_name
+      customer.last_name.downcase.include?(last_name.downcase)
     end
   end
 
